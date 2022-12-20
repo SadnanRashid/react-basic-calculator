49039c712306d7ee281b40f8bf3bf72f0894c950
SadnanRashid <sadnan.rashid.07@gmail.com>
2022-12-20 20:11:54 +0600

    uplaod

--- src/App.js
+++ src/App.js
@@ -1,23 +1,164 @@
-import logo from './logo.svg';
-import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState("");
+  // const [error, setError] = useState("");
+  const doMath = (mathString) => {
+    try {
+      const mathResult = eval(mathString);
+      console.log(mathResult);
+      setNumber(mathResult);
+    } catch (error) {
+      console.log(error);
+      setNumber("Error input!");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="calculator-body">
+      <input type="text" className="black-screen" value={number} disabled />
+
+      <div className="calculator-buttons">
+        {/* Operator buttons */}
+        <button
+          className="operator-button btn btn-dark"
+          value="+"
+          onClick={() => setNumber(number + "+")}
         >
-          Learn React
-        </a>
-      </header>
+          +
+        </button>
+        <button
+          className="operator-button btn btn-dark"
+          value="-"
+          onClick={() => setNumber(number + "-")}
+        >
+          -
+        </button>
+        <button
+          className="operator-button btn btn-dark"
+          value="*"
+          onClick={() => setNumber(number + "*")}
+        >
+          x
+        </button>
+        <button
+          className="operator-button btn btn-dark"
+          value="/"
+          onClick={() => setNumber(number + "/")}
+        >
+          /
+        </button>
+      </div>
+      <div className="calculator-numbers">
+        <button
+          type="button"
+          value="7"
+          className="btn btn-info"
+          onClick={() => setNumber(number + "7")}
+        >
+          7
+        </button>
+        <button
+          type="button"
+          value="8"
+          className="btn btn-info"
+          onClick={() => setNumber(number + "8")}
+        >
+          8
+        </button>
+        <button
+          type="button"
+          value="9"
+          className="btn btn-info"
+          onClick={() => setNumber(number + "9")}
+        >
+          9
+        </button>
+        <button
+          type="button"
+          value="4"
+          className="btn btn-info"
+          onClick={() => setNumber(number + "4")}
+        >
+          4
+        </button>
+        <button
+          type="button"
+          value="5"
+          className="btn btn-info"
+          onClick={() => setNumber(number + "5")}
+        >
+          5
+        </button>
+        <button
+          type="button"
+          value="6"
+          className="btn btn-info"
+          onClick={() => setNumber(number + "6")}
+        >
+          6
+        </button>
+        <button
+          type="button"
+          value="1"
+          className="btn btn-info"
+          onClick={() => setNumber(number + "1")}
+        >
+          1
+        </button>
+        <button
+          type="button"
+          value="2"
+          className="btn btn-info"
+          onClick={() => setNumber(number + "2")}
+        >
+          2
+        </button>
+        <button
+          type="button"
+          value="3"
+          className="btn btn-info"
+          onClick={() => setNumber(number + "3")}
+        >
+          3
+        </button>
+        <button
+          type="button"
+          value="0"
+          className="btn btn-info"
+          onClick={() => setNumber(number + "0")}
+        >
+          0
+        </button>
+        <button
+          type="button"
+          value="="
+          className="btn btn-info w-100"
+          onClick={() => setNumber(number + ".")}
+        >
+          .
+        </button>
+        <button
+          type="button"
+          value="="
+          className="btn btn-warning w-100"
+          onClick={() => setNumber("")}
+        >
+          C
+        </button>
+      </div>
+      <div className="equal-btn mb-1 px-3">
+        <button
+          type="button"
+          value="="
+          className="btn btn-danger w-100"
+          onClick={() => doMath(number)}
+        >
+          =
+        </button>
+      </div>
     </div>
   );
 }
